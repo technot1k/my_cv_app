8f383067ee4a6f9d183ae283a7dd2956c7478f7e
sky <khakhalev87@gmail.com>
2025-02-12 19:44:12 +0300

    add assets

--- lib/main.dart
+++ lib/main.dart
@@ -22,19 +22,25 @@ class MyCVScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Text('D1ma H',
-                style: TextStyle(
-                    fontSize: 40,
-                    fontFamily: 'Lobster',
-                    fontWeight: FontWeight.bold)),
-            Text('Flutter Developer')
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              CircleAvatar(
+                radius: 90,
+                foregroundImage: AssetImage('assets/1.jpg'),
+              ),
+              Text('D1ma H',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'Lobster',
+                      fontWeight: FontWeight.bold)),
+              Text('Flutter Developer')
+            ],
+          ),
         ),
       ),
     );
