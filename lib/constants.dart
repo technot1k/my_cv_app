4aadbe2b873461976d0fb1d14e2e8f6b945319ed
sky <khakhalev87@gmail.com>
2025-02-12 22:55:36 +0300

    finish

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const TextStyle smallTextStyle = TextStyle(fontFamily: 'Lobster', fontSize: 16);
+const TextStyle regularTextStyle = TextStyle(fontFamily: 'Lobster');
+const TextStyle regularBoltTextStyle =
+    TextStyle(fontFamily: 'Lobster', fontWeight: FontWeight.w700);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_cv_app/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,9 +27,7 @@ class MyCVScreen extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              SizedBox(
-                height: 30,
-              ),
+              SizedBox(height: 30),
               CircleAvatar(
                 radius: 90,
                 foregroundImage: AssetImage('assets/1.jpg'),
@@ -38,7 +37,68 @@ class MyCVScreen extends StatelessWidget {
                       fontSize: 40,
                       fontFamily: 'Lobster',
                       fontWeight: FontWeight.bold)),
-              Text('Flutter Developer')
+              Text('Flutter Developer'),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(Icons.call),
+                    title: Text('+79187777777', style: smallTextStyle),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text('email.gmail.com', style: smallTextStyle),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Education',
+                            textAlign: TextAlign.center,
+                            style: regularBoltTextStyle,
+                          ),
+                          Text('NPI, Novocherkassk',
+                              textAlign: TextAlign.center,
+                              style: regularTextStyle),
+                          Text('University',
+                              textAlign: TextAlign.center,
+                              style: regularTextStyle),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text('Work experience',
+                              textAlign: TextAlign.center,
+                              style: regularBoltTextStyle),
+                          Text('English',
+                              textAlign: TextAlign.center,
+                              style: regularTextStyle),
+                          Text('Flutter developer',
+                              textAlign: TextAlign.center,
+                              style: regularTextStyle),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
